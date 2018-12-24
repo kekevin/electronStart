caabadeb88f75fda7726fdc712c89f9fd2aa345a
k.pc <linkkevin@qq.com>
2018-12-24 16:30:33 +0800

    test
    test

---
+++ src/renderer/page/index/welcome.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="box w h">
+    aaa12231222
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.box{
+    color: red;
+    // background-image: url('./../../assets/img/welcome.jpg');
+    // background-repeat: no-repeat;
+    // background-size: 100% 100%;
+}
+</style>
+
